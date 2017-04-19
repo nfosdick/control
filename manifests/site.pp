36f35ec14fbc4105c4d5d6cfa4197d710fdac81a
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-04-19 09:50:05 -0600

    testing

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
 #  $role = $trusted['extensions']['pp_role']
-  $nick_role = $trusted['extensions']['myshortname']
+  $nick_role = $trusted['extensions']['1.3.6.1.4.1.34380.1.2.1.1']
   notify{"Nick ${nick_role}":}
   if ($::role) {
     include "role::${::role}"
